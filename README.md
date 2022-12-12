29bd2a718787d5dbde7994fcb14eaa0c1fd7d076
Sasha <sasha.svetogor@gmail.com>
2022-12-12 18:02:38 +0300

    added image app in readme file

--- README.md
+++ README.md
@@ -44,3 +44,16 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+![img.png](img.png)
+![img_1.png](img_1.png)
+![img_2.png](img_2.png)
+![img_3.png](img_3.png)
+![img_4.png](img_4.png)
+![img_5.png](img_5.png)
+![img_6.png](img_6.png)
+![img_7.png](img_7.png)
+![img_8.png](img_8.png)
+![img_9.png](img_9.png)
+![img_10.png](img_10.png)
+если нажать кнопку сегодня, выделится сегодняшний день
+![img_11.png](img_11.png)
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from "../../containers/Header/Header";
 import './Profile.css'
 import {useTypedSelector} from "../../redux/hooks/useTypedSelector";
+import {useDispatch} from "react-redux";
+import {fetchEvents} from "../../redux/actions/eventActionCreator/eventActionCreator";
 
 const Profile = () => {
     const {user} = useTypedSelector(state => state.auth)
+    const {events} = useTypedSelector(state => state.events)
+    const dispatch = useDispatch<any>()
+
+    useEffect(() => {
+        dispatch(fetchEvents(user.username))
+        console.log('profileevent', events)
+    }, [])
     return (
         <div>
             <Header />
@@ -21,6 +30,16 @@ const Profile = () => {
                     {user.password.slice(0,4)}...
                 </span>
             </p>
+            <div className='profile-events-wrapper'>
+                <p className='profile-event-text'>Ваши события: </p>
+                {events.map(ev => {
+                    return <div className='profile__event'>
+                        <p className='event-text'>{ev.date}</p>
+                        <h3 className='event-title'>{ev.title}</h3>
+                        <p className='event-text'>{ev.guests}</p>
+                    </div>
+                })}
+            </div>
         </div>
     );
 };
